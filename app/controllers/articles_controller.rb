99c10d70c8d65a24653736f31c983e88fa3116b5
Corey Garvey <CGarv16@gmail.com>
2014-09-01 21:00:22 -0700

    Ability to edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,20 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(article_params)
+      flash[:success] = "Article updated"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def article_params
