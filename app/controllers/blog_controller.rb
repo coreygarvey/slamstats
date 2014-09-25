04dc86d79c0e83bfb8c6362565159219b97824ff
Corey Garvey <CGarv16@gmail.com>
2014-09-25 02:24:58 -0700

    new landing page

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,6 +1,8 @@
 class BlogController < ApplicationController
   def home
-    @article = Article.order('created_at desc').first
+    @art1 = Article.order('created_at desc').first
+    @art2 = Article.find_by_id(1)
+    @art3 = Article.find_by_id(3)
   end
 end
  
